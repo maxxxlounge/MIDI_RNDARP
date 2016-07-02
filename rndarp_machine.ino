a0ffbb8bdc9501e32f583787989a5b06323b9acb
maxxlounge <maxxlounge@gmail.com>
2016-07-02 17:27:32 +0200

    superpulizia

--- rndarp_machine.ino
+++ rndarp_machine.ino
@@ -1,10 +1,14 @@
+#include <LiquidCrystal.h>
+
 
 /**
   DEFINISCO LE COSTANTI PER LE LETTURE DEGLI INPUT
 */
-const int PIN_SEED = -1;     
-const int PIN_TEMPO_INC = -1;     
-const int PIN_TEMPO_DEC = -1;
+const int PIN_VAL = 7; 
+const int PIN_MODE = 10;
+const int PIN_INC = 9;     
+const int PIN_DEC = 8;
+
 
 //FUNZIONI DI MENU
 int sensorMin = 0;
@@ -14,30 +18,22 @@ int range = 1;
 int octave = 5;
 int tonalita = 1; //1=C, 2=C#, 12=B etc...
 int tempo = 120;
-int velocity[] = {127,127,127,127,127,127,127,127};
 int tones[] = {60,60,60,60,60,60,60,60};
-int rndArray[8];
-int currentNotes[8];
 int midi_ch=1;
-int *ptones;
-boolean play = false;
 
 int currentMenu = 0;
 
-String currentMode = "TEMPO";
-
-String scale = "C";
-int maggiore[] = {2,2,1,2,2,2,1};
+int maggiore[] = {3,5,6,8,10,12,13};
+int minore[] = {3,4,6,8,9,11,13};
+/*
 int dorico[] = {2,1,2,2,2,1,2};
 int frigio[] = {1,2,2,2,1,2,2};
 int lidio[] = {2,2,2,1,2,2,1};
 int misolidio[] = {2,2,1,2,2,1,2};
-int minore[] = {2,1,2,2,1,2,2};
+
 int locrio[] = {1,2,2,1,2,2,2};
 int pentatonic[] = {1,2,3,2,2,3,1};
-
-
-int ret[8];
+*/
 
 float bps = tempo/60;
 float tickDuration = 1/bps;
@@ -47,16 +43,21 @@ int arp_tick = del_tick / (battute/4);
 int menuReading;
 int changeReading;
 
+
 boolean seed_btn;
 boolean tempo_inc;
 boolean tempo_dec;   
+String menuLabel;
 
+ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+/*
 void noteOn(int cmd, int pitch, int velocity) {
   Serial.write(cmd);
   Serial.write(pitch);
   Serial.write(velocity);
 }
-
+*/
+/*
 void scala(int initNote,int modo[], int range)
 {
   int modoRange[]={};
@@ -71,7 +72,7 @@ void scala(int initNote,int modo[], int range)
     //  for (n=0;n<sizeOf(modo);n++){
     //    modoRange[r*7+n] = modo[n];
     //  }
-  */
+ 
 
   ret[0] = initNote;
   int i;
@@ -79,38 +80,35 @@ void scala(int initNote,int modo[], int range)
      ret[i+1] = initNote+modoRange[i];
   } 
 }
+*/
 
 //Restituisce una rray di battute Randomizza gli array nelle battute (8)
-void randomize(int rnd_min, int rnd_max){
+/*void randomize(int rnd_min, int rnd_max){
   for(int i=0;i<battute;i++){
     int rnd = random(rnd_max-rnd_min);
     rndArray[i] = rnd + rnd_min;
   }
-}
+}*/
 
 void setup() {
-  //currentMode = pentatonic
-  //range = 1
-  //scala = scala(60,currentMode,range);
-  play=true;
-  // set up the LCD's number of columns and rows:
-  //lcd.begin(16, 2);
-  // Print a message to the LCD.
-  //lcd.print("RNDARP MACHINE");
-  //Serial.begin(9600);
-  Serial.begin(31250);
+  lcd.begin(16, 2);
+  lcd.print("RNDARP MACHINE");
+  Serial.begin(9600);
+  pinMode(PIN_INC,INPUT_PULLUP);
+  pinMode(PIN_DEC,INPUT_PULLUP);
+  pinMode(PIN_MODE,INPUT_PULLUP);
+  pinMode(PIN_VAL,INPUT_PULLUP);  
+  currentMenu=0;
+  //Serial.begin(31250);
 }
 
 ///play the rnd arp
-void playSound(){
+/*void playSound(){
   for (int i=0;i<8;i=i+1){
     String space = " ";
     Serial.println(tones[0] + space + velocity[i] + space + midi_ch);
   }
-}
-
-String menuLabel = "";
-int lastMenu = 0;
+}*/
 
 void updateTempo(){
   bps = tempo/60;
@@ -120,21 +118,48 @@ void updateTempo(){
   arp_tick = del_tick / (battute/4);
 } 
 
+void setMenuLabel(String label){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(label);
+//      lcd.setCursor(0,1);
+    }
+
+
 void inputReading(){
-    menuReading = analogRead(A0);
-    changeReading = analogRead(A1);
-    seed_btn = digitalRead(PIN_SEED);
-    tempo_inc = digitalRead(PIN_TEMPO_INC);
-    tempo_dec = digitalRead(PIN_TEMPO_DEC);    
-    if (tempo_inc){
-      tempo += 1;
-    }
-    if (tempo_dec){
-      tempo -= 1;
-    }
-    if (tempo_dec || tempo_inc){
-      updateTempo();
-    }
+    menuReading = digitalRead(PIN_MODE);
+    changeReading = digitalRead(PIN_VAL);
+    if (menuReading==0){
+      if (currentMenu>=6){
+        currentMenu=0;
+      }else{
+        currentMenu+=1;
+      } 
+
+      switch (currentMenu) {
+        case 0:    // VELOCITY
+          menuLabel = "VELOCITY";
+          break;
+        case 1:    // TONE
+          menuLabel = "TONE";
+          break;
+        case 2:    // SCALE
+          menuLabel = "SCALE";
+          break;  
+        case 3: //RANGE
+          menuLabel = "RANGE";
+          break;
+        case 4: //OCTAVE
+          menuLabel = "OCTAVE";
+          break;
+        case 5: //TEMPO
+          menuLabel = "TEMPO";
+          break;
+      }    
+      setMenuLabel(menuLabel);
+      delay(300);
+    
+  }
 }
 
 void seed(){
@@ -146,52 +171,55 @@ void seed(){
   }
 }
 
+
 void loop() {
-  
-  // map the sensor range to a range of four options:
-  int range = map(menuReading, sensorMin, sensorMax, 0, 6);
-  currentMenu=range;
+  inputReading();
   switch (currentMenu) {
     case 0:    // VELOCITY
-      menuLabel = "VELOCITY";
+      delay(300);
       break;
     case 1:    // TONE
-      menuLabel = "TONE";
+      delay(300);    
       break;
     case 2:    // SCALE
-      menuLabel = "SCALE";
+      delay(300);    
       break;  
     case 3: //RANGE
-      menuLabel = "RANGE";
-      range = map(changeReading, sensorMin, sensorMax, 1, 4);
+      lcd.setCursor(0,1);
+      lcd.print(String(range));
+    
+      delay(300);
       break;
     case 4: //OCTAVE
-      menuLabel = "OCTAVE";
-      octave = map(changeReading, sensorMin, sensorMax, 1, 8);
+      lcd.setCursor(0,1);
+      lcd.print(String(octave));
+      delay(300);
       break;
+    case 5: //TEMPO
+      tempo_inc = digitalRead(PIN_INC);
+      tempo_dec = digitalRead(PIN_DEC);    
+      if (tempo_inc==0){
+        tempo += 1;
+      }
+      if (tempo_dec==0){
+        tempo -= 1;
+      }
+      if (tempo_dec==0 || tempo_inc==0){
+        updateTempo();
+      }
+      lcd.setCursor(0,1);
+      lcd.print(String(tempo) + " BPM ");
+      delay(100);
+      break;
+    case 6:    // PLAY
+      for (int b=0;b<battute;b++){
+        //inputReading();
+        //noteOn(0x90, tones[b], velocity[b]);
+        Serial.print(String(tones[b],HEX)+"|" );
+        delay(arp_tick);
+        //noteOn(0x90,tones[b], 0x00);
+      }
+      break;    
   }
   
-  if (lastMenu != currentMenu){
-    Serial.println(menuLabel);
-    lastMenu = currentMenu;
-  }
-  
-  Serial.println(tempo);
-  //Serial.println(del_tick);
-  if (play==true){
-  for (int b=0;b<battute;b++){
-    inputReading();
-    noteOn(0x90, tones[b], velocity[b]);
-    Serial.print(String(tones[b],HEX)+"|" );
-    delay(arp_tick);
-    noteOn(tones[b], note, 0x00);
-  }
-  Serial.println();
-  }
-  
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  //lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  //lcd.print(millis() / 1000);
 }
