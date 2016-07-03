1047e1cf14fbb85a52908515ee02de2348b47cd9
maxxlounge <maxxlounge@gmail.com>
2016-07-03 09:30:47 +0200

    aggiunta monotono digital pin

--- rndarp_machine.ino
+++ rndarp_machine.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 
+float freqs[] = {16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87,32.7,34.65,36.71,38.89,41.2,43.65,46.25,49,51.91,55,58.27,61.74,65.41,69.3,73.42,77.78,82.41,87.31,92.5,98,103.83,110,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185,196,207.65,220,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392,415.3,440,466.16,493.88,523.25,554.37,587.33,622.25,659.25,698.46,739.99,783.99,830.61,880,932.33,987.77,1046.5,1108.73,1174.66,1244.51,1318.51,1396.91,1479.98,1567.98,1661.22,1760,1864.66,1975.53,2093,2217.46,2349.32,2489.02,2637.02,2793.83,2959.96,3135.96,3322.44,3520,3729.31,3951.07,4186.01,4434.92,4698.63,4978.03,5274.04,5587.65,5919.91,6271.93,6644.88,7040,7458.62,7902.13};
 
 /**
   DEFINISCO LE COSTANTI PER LE LETTURE DEGLI INPUT
@@ -18,6 +19,7 @@ int sensorMax = 1024;
 int battute = 8;
 int range = 1;
 int octave = 5;
+int popolation = 80;
 int note = 0; //1=C, 2=C#, 12=B etc...
 String note_str[] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
 
@@ -57,6 +59,10 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 String noteToString(int note_local){
   return note_str[(note_local % 12)];
+}
+
+float getFreq(int note_int){
+  return freqs[note_int];
 }
 
 void setRndNotes(){
@@ -182,7 +188,11 @@ void randomize(){
             if (scale>1){
               rndnote = rnd_notes[random(0,7)];
             }
+            if (random(0,100)<popolation){
             tones[b]=random(octave,octave+range)*12+rndnote;
+            }else{
+              tones[b]=-1;
+            }
           }
 }    
 
@@ -195,7 +205,7 @@ void inputReading(){
     }
     if (menuReading==0){
       lcd.clear();
-      if (currentMenu>=6){
+      if (currentMenu>=7){
         currentMenu=0;
       }else{
         currentMenu+=1;
@@ -220,7 +230,10 @@ void inputReading(){
         case 5: //TEMPO
           menuLabel = "TEMPO";
           break;
-        case 6: //PLAY
+        case 6: //POPLATION
+          menuLabel = "POPLATION";
+          break;
+        case 7: //PLAY
           menuLabel = "PLAY";
           randomize();
           break;
@@ -297,11 +310,21 @@ void loop() {
       lcd.print(String(tempo) + " BPM ");
       delay(100);
       break;
-    case 6:    // PLAY
+    case 6: // POPULATION
+      incDecManage(popolation,1,100);
+      lcd.print(popolation);
+      delay(100);
+      break;
+    case 7:    // PLAY
       String out = "";
       for (int b=0;b<battute;b++){
         //noteOn(0x90, tones[b], velocity[b]);
         out+= String(tones[b])+"|";
+        if (tones[b]>0){
+          double freq = getFreq(tones[b]);
+          tone(6,freq,arp_tick);
+        //noTone(6);
+        }
         delay(arp_tick);
         //noteOn(0x90,tones[b], 0x00);
       }
